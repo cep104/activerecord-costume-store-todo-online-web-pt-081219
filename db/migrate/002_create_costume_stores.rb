e3ef08144242d59784797d410accbf1327815423
Candice Peters <candicelizabethpeters@gmail.com>
2019-11-15 04:31:24 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventroy
+      t.integer :num_of_employees
+      t.string :opening_time
+      t.string :closing_time
+  end
+end
 # Create your costume_stores migration here
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
+class CreateHauntedHouse < ActiveRecord::Migration[5.1]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friends
+      t.string :opening_date
+      t.string :closing_date
+      t.string :long_description
+  end
+end
 # Create your haunted_houses migration here
