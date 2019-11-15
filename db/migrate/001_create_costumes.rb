80a09c68bee91642f3675ca2f6e7c2c825fcad28
Candice Peters <candicelizabethpeters@gmail.com>
2019-11-15 04:36:10 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -7,6 +7,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.string :image_url
       t.timestamp :created_at
       t.timestamp :updated_at
+    end
   end
 end
 # Create a class and inherit from ActiveRecord::Migration
